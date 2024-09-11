ca5c16d8b7a380fdef3ffb4b4d5723c0dbabc3d0
adpthegreat <adpthegreatanddefiant@gmail.com>
2024-09-10 22:19:16 -0400

    - Feat : changed the type for unlock_price from f64 to u64 - Renamed escrow to escrow_state - Renamed all variables with _ix to _instruction - Renamed tx to transaction - Removed Magic Number (8) and replaced with ANCHOR_DISCRIMINATOR const - changed std::mem::size_of::<Escrow>() to Escrow::INIT_SPACE - Wrapped solPrice with BN - Updated assertions in tests to use .eq() method

--- programs/burry-escrow/src/lib.rs
+++ programs/burry-escrow/src/lib.rs
@@ -1,20 +1,20 @@
 use anchor_lang::prelude::*;
 use instructions::deposit::*;
 use instructions::withdraw::*;
-use state::*;
+
 
 pub mod errors;
 pub mod instructions;
 pub mod state;
 
-declare_id!("9Wm9QwzaAdKHz6Pt2jQ6bLG4ssjXnwvcXqiaWvcCSLkQ");
+declare_id!("3V8LV4FgZCJY6qpRp8XpLGbhdJW9pWLHyXpTs6PRfqzE");
 
 #[program]
 pub mod burry_escrow {
     use super::*;
 
-    pub fn deposit(ctx: Context<Deposit>, escrow_amt: u64, unlock_price: f64) -> Result<()> {
-        deposit_handler(ctx, escrow_amt, unlock_price)
+    pub fn deposit(ctx: Context<Deposit>, escrow_amount: u64, unlock_price: u64) -> Result<()> {
+        deposit_handler(ctx, escrow_amount, unlock_price)
     }
 
     pub fn withdraw(ctx: Context<Withdraw>) -> Result<()> {
--- programs/burry-escrow/src/state.rs
+++ programs/burry-escrow/src/state.rs
@@ -4,7 +4,10 @@ pub const ESCROW_SEED: &[u8] = b"MICHAEL BURRY";
 pub const SOL_USDC_FEED: &str = "GvDMxPzN1sCj7L26YDK2HnMRXEQmQ2aemov8YBtPS7vR";
 
 #[account]
-pub struct EscrowState {
-    pub unlock_price: f64,
+#[derive(InitSpace)]
+pub struct Escrow {
+    pub unlock_price:u64,
     pub escrow_amount: u64,
 }
+
+
